5d2a9ab176444e7d39f807d8b444986f08137ddc
farazcity <90553482+farazcity@users.noreply.github.com>
2021-12-02 23:35:35 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,request,jsonify
 from models import *
 
+import threading,queue
+
 app=Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"]="postgres://postgres:postgres@localhost:5432/postgres"
@@ -34,4 +36,81 @@ def flight(flight_id):
 	passengers=Passenger.query.filter_by(flight_id=flight_id)
 	return render_template("flight.html",flight=flight,passengers=passengers)
 
+@app.route("/min")
+def min():
+	return render_template("min.html")
+
+@app.route("/min/minimum")
+def minimum():
+	source=request.form.get("source")
+	destination=request.form.get("destination")
+	flights=Flight.query.all()
+
+
+	class graph:
+
+		def insert(self,airports):
+			self.adj_list={}
+			for a in airports:
+				origin,destination=a.split(":")
+				if origin not in self.adj_list:
+					self.adj_list[origin]=[destination]
+				else:
+					self.adj_list[origin].append(destination)
+
+		def display_adj_list(self):
+			print(self.adj_list)
+
+		def bfs(self,source):
+			self.visited={}
+			self.parent={}
+			self.level={}
+			self.traversal=[]
+			for i in self.adj_list:
+				self.visited[i]=False
+				self.parent[i]=None
+				self.level[i]=-1
+			self.visited[source]=True
+			self.level[source]=0
+			q=queue.Queue()
+			q.put(source)
+			while not q.empty():
+				u=q.get()
+				self.traversal.append(u)
+				for v in self.adj_list[u]:
+					self.visited[v]=True
+					self.parent[v]=u
+					self.level[v]=self.level[u]+1
+					q.put(v)
+
+		def shortest_path(q):
+			l=[]
+			l.append(q)
+			if self.parent(q)!=None:
+				q=q.parent
+			l.reverse()
+			return(l)
+
+		def bfs_data(self,data):
+			def visited(self,data):
+				return self.visited[data]
+			def parent(self,data):
+				return self.parent[data]
+			def level(self,data):
+				return self.level[data]
+			def traversal(self):
+				return self.traversal
+
+
+	obj=graph()
+	a=[]
+	for i in flights:
+		a.append(i.origin,i.destination)
+	obj.insert(a)
+	#obj.display_adj_list()
+	obj.bfs("source")
+	#print(obj.bfs_data.traversal())
+	sp=obj.shortest_path("destination")
+	
+	return render_template("minimum.html",minimun=sp)
 
